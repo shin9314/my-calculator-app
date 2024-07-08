9a259a7b39caeaaa21aef038ca62e27398ce398d
shin9314 <tikenoxue@gmail.com>
2024-07-08 13:39:42 +0900

    Add calculator functionality

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Calculator from '../components/Calculator';
+
+export default function Home() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <Calculator />
+    </div>
+  );
+}
